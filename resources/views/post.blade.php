0b8cae6572c32b5f34fedebe967648c0bd1d1626
Cahyooo <cahyawinata1212@gmail.com>
2023-07-29 22:51:46 +0700

    melanjutkan pembelajaran

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,20 @@
 
 @section('container')
     <article>
-        <h2>{{ $post['title'] }}</h2>
-        <h5>{{ $post['arthor'] }}</h5>
-        <p>{{ $post['body'] }}</p>
+        <h2>{{ $post->title }}</h2>
+
+        {!! $post->body !!}
+
     </article>
 
     <a href="/posts">Back To Post</a>
 @endsection
+
+{{-- 
+    Post::create([
+    'title' => 'Judul Ke Tiga',
+    'slug' => 'judul-ke-tiga',
+    'excerpt' => 'Lorem ipsum dolor sit amet consectetur ke tiga',
+    'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti voluptas possimus, quas suscipit consequuntur quae atque dolorum. Dolores ipsam eveniet sunt molestiae distinctio ducimus voluptatibus obcaecati reiciendis ratione quisquam perferendis quia vel laborum error non officia, aliquid quae beatae molestias maxime et? Doloremque error veniam autem repellendus quod tenetur molestias qui illo laborum! Aliquid fuga excepturi ducimus incidunt? Necessitatibus illum aspernatur, consequuntur eveniet aperiam ut odio! Deleniti porro tenetur similique.</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsum pariatur totam voluptates modi consectetur iure iste, nisi sapiente culpa sed ipsam expedita officia, eum voluptas tempore molestiae neque reiciendis delectus enim molestias sint quia dignissimos nostrum! Animi, quibusdam. Veniam voluptas, cum mollitia nisi temporibus incidunt ex harum eum ea!</p>'
+]) 
+--}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,10 @@
 
 @section('container')
     @foreach ($posts as $post)
-        <article class="mb-5">
-            <h2><a href="/posts/{{ $post['id'] }}">{{ $post['title'] }}</a></h2>
-            <h5>By:{{ $post['arthor'] }}</h5>
-            <p>{{ $post['body'] }}</p>
+        <article>
+            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 @endsection
 
-{{-- 12:44 --}}
