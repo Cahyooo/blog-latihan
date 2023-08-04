be3e17f851087186344baed7dce376adf9b19640
Cahyooo <cahyawinata1212@gmail.com>
2023-08-04 21:21:26 +0700

    vidio ke 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Sandhika Galih',
+            'email' => 'sandhikagalih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Doddy Ferdiansyah',
+            'email' => 'doddyferdiansyah@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, debitis excepturi nostrum perferendis suscipit provident sint, nemo praesentium velit exercitationem id. Fuga laudantium voluptates similique qui maxime sunt voluptatibus, vel repudiandae quis! Rem autem illum unde, hic quis laborum provident ipsa molestias, omnis eius reprehenderit officia fuga libero? Similique dolore amet error vitae perspiciatis quisquam, illum quos quod repellat iusto facilis dignissimos suscipit ab culpa et nemo magni unde veritatis sapiente excepturi explicabo sint eius minus. Voluptas, autem. Distinctio amet, earum ipsam et quae odit voluptatibus dolor facilis corporis voluptate perspiciatis, blanditiis aspernatur nostrum repudiandae itaque id fugiat. Reiciendis, perspiciatis!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, debitis excepturi nostrum perferendis suscipit provident sint, nemo praesentium velit exercitationem id. Fuga laudantium voluptates similique qui maxime sunt voluptatibus, vel repudiandae quis! Rem autem illum unde, hic quis laborum provident ipsa molestias, omnis eius reprehenderit officia fuga libero? Similique dolore amet error vitae perspiciatis quisquam, illum quos quod repellat iusto facilis dignissimos suscipit ab culpa et nemo magni unde veritatis sapiente excepturi explicabo sint eius minus. Voluptas, autem. Distinctio amet, earum ipsam et quae odit voluptatibus dolor facilis corporis voluptate perspiciatis, blanditiis aspernatur nostrum repudiandae itaque id fugiat. Reiciendis, perspiciatis!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, debitis excepturi nostrum perferendis suscipit provident sint, nemo praesentium velit exercitationem id. Fuga laudantium voluptates similique qui maxime sunt voluptatibus, vel repudiandae quis! Rem autem illum unde, hic quis laborum provident ipsa molestias, omnis eius reprehenderit officia fuga libero? Similique dolore amet error vitae perspiciatis quisquam, illum quos quod repellat iusto facilis dignissimos suscipit ab culpa et nemo magni unde veritatis sapiente excepturi explicabo sint eius minus. Voluptas, autem. Distinctio amet, earum ipsam et quae odit voluptatibus dolor facilis corporis voluptate perspiciatis, blanditiis aspernatur nostrum repudiandae itaque id fugiat. Reiciendis, perspiciatis!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, debitis excepturi nostrum perferendis suscipit provident sint, nemo praesentium velit exercitationem id. Fuga laudantium voluptates similique qui maxime sunt voluptatibus, vel repudiandae quis! Rem autem illum unde, hic quis laborum provident ipsa molestias, omnis eius reprehenderit officia fuga libero? Similique dolore amet error vitae perspiciatis quisquam, illum quos quod repellat iusto facilis dignissimos suscipit ab culpa et nemo magni unde veritatis sapiente excepturi explicabo sint eius minus. Voluptas, autem. Distinctio amet, earum ipsam et quae odit voluptatibus dolor facilis corporis voluptate perspiciatis, blanditiis aspernatur nostrum repudiandae itaque id fugiat. Reiciendis, perspiciatis!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,13 +4,13 @@
     <article>
         <h2>{{ $post->title }}</h2>
 
-        <p>By Sandhika Galih in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
 
     </article>
 
-    <a href="/posts">Back To Post</a>
+    <a href="/posts" class="d-block mt-3">Back To Post</a>
 @endsection
 
 {{-- 
